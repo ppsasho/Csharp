8710ce64cce2added8a45891d8f38500891fa9af
ppsasho <businessmail5545@gmail.com>
2024-02-28 20:55:54 +0100

    - schoolwork

---
+++ schoolWork/class 02/readConsole&Parse/readConsole&Parse/readConsole&Parse/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace readConsole_Parse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a num");
+            string input = Console.ReadLine();
+
+            bool success = int.TryParse(input, out int result );
+
+            if(!success)
+            {
+                Console.WriteLine("Wrong input!");
+            }
+            Console.WriteLine($"Using TryParse: {result}");
+
+            //int newNum = Convert.ToInt32(input);
+            //Console.WriteLine($"Using Convert: {newNum}");
+
+            if (result > 0) 
+            {
+                Console.WriteLine("It's a number!");
+            } else {
+                Console.WriteLine("It's not a number!");
+            }
+            
+            
+        }
+    }
+}
