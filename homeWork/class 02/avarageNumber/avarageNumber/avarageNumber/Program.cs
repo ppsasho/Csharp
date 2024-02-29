3ebd41d66abcfe0e6abc9131f532cf0d7a7c4aa7
ppsasho <businessmail5545@gmail.com>
2024-02-29 16:14:58 +0100

    - fixed avarageNumbers

--- homeWork/class 02/avarageNumber/avarageNumber/avarageNumber/Program.cs
+++ homeWork/class 02/avarageNumber/avarageNumber/avarageNumber/Program.cs
@@ -4,10 +4,23 @@
     {
         static void Main(string[] args)
         {
-            int firstNum = 10;
-            int secondNum = 15;
-            int thirdNum = 20;
-            int fourthNum = 30;
+            Console.WriteLine("Enter 4 numbers:");
+            string firstInput = Console.ReadLine();
+            string secondInput = Console.ReadLine();
+            string thirdInput = Console.ReadLine();
+            string fourthInput = Console.ReadLine();
+
+            int secondNum = 0;
+            int thirdNum = 0;
+            int fourthNum = 0;
+            bool fourNumbers = 
+                int.TryParse(firstInput, out int firstNum) && int.TryParse(secondInput, out secondNum)
+              & int.TryParse(thirdInput, out thirdNum) && int.TryParse(fourthInput, out fourthNum);
+            if(!fourNumbers)
+            {
+                Console.WriteLine("You didn't enter four numbers!");
+                return;
+            }
             int sum = (firstNum + secondNum + thirdNum + fourthNum) / 4;
             Console.WriteLine($"The avarage of {firstNum}, {secondNum}, {thirdNum} and {fourthNum} is: {sum}");
         }
--- homeWork/class 02/avarageNumber/avarageNumber/avarageNumber/obj/Debug/net8.0/avarageNumber.AssemblyInfo.cs
+++ homeWork/class 02/avarageNumber/avarageNumber/avarageNumber/obj/Debug/net8.0/avarageNumber.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("avarageNumber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8710ce64cce2added8a45891d8f38500891fa9af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f664153450fcb1de06dc49c9a7f8e223f6a3a51f")]
 [assembly: System.Reflection.AssemblyProductAttribute("avarageNumber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("avarageNumber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
