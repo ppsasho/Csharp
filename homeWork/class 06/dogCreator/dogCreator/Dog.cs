977b4d37d436b060554c7870fd8c01d8f2ca746e
ppsasho <businessmail5545@gmail.com>
2024-03-12 14:09:58 +0100

    - homework

---
+++ homeWork/class 06/dogCreator/dogCreator/Dog.cs
@@ -0,0 +1,28 @@
+﻿namespace dogCreator
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+
+        public Dog(string name, string breed, string color)
+        {
+            Name = name;
+            Breed = breed;
+            Color = color;
+        }
+        public string Eat()
+        {
+            return $"The dog {Name} is now eating.";
+        }
+        public string Play()
+        {
+            return $"The dog {Name} is now playing";
+        }
+        public string ChaseTail()
+        {
+            return $"The dog {Name} is chasing it's tail.";
+        }
+    }
+}
---
+++ homeWork/class 06/dogCreator/dogCreator/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace dogCreator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog; 
+            while (true)
+            {
+                dog = new Dog(GetInput("Enter your dog's name:"), GetInput("Enter your dog's breed:"), GetInput("Enter your dog's color:"));
+                Console.WriteLine(dog.Name, dog.Breed, dog.Color);
+                if (dog.Name == "" ||
+                    dog.Breed == "" ||
+                    dog.Color == "")
+                {
+                    Console.WriteLine("Please make sure you fill in all the fields");
+                    continue;
+                }
+
+                    break;
+            }
+
+            while (true)
+            {
+                string choice = GetInput("Choose a number from 1 to 3:");
+                switch(choice)
+                {
+                    case "1":
+                        Console.WriteLine(dog.Eat());
+                        return;
+
+                    case "2":
+                        Console.WriteLine(dog.Play());
+                        return;
+
+                    case "3":
+                        Console.WriteLine(dog.ChaseTail());
+                        return;
+
+                    default:
+                        Console.WriteLine("Please make sure you enter one number.");
+                        continue;
+                }
+            }
+        }
+        public static string GetInput(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+
+            return input;
+        }
+    }
+}
